b56095221013f84c2f68dd3d2ca39e8a1017c702
UCat07 <soo.zw04@gmail.com>
2024-12-23 21:02:08 +0800

    Update GameLogic.java

--- GameLogic.java
+++ GameLogic.java
@@ -3,18 +3,16 @@
 class GameLogic {
     private Board board;
     private boolean blueTurn;
-    private ChessController ctr;  // Reference to the ChessController to update the display
 
-    public GameLogic(Board board, ChessController ctr) {
+    public GameLogic(Board board) {
         this.board = board;
-        this.ctr = ctr;  // Pass the ChessController reference
         this.blueTurn = true;
     }
 
     public void switchTurn() {
         blueTurn = !blueTurn;
         board.rotate(); // Rotate the board after each turn
-        board.updateBoardDisplay();  // Notify Controller to update the visual display after rotation
+        board.updateBoardDisplay();  // Notify GUI to update the visual display after rotation
     }
 
     public boolean isBlueTurn() {
