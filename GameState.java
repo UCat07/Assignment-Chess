4540bf653dbd4c0cdb1ffcaba7d17048ba97c386
SiaoWeiCheng <siaoweicheng@gmail.com>
2024-12-24 23:58:41 +0800

    Update GameState.java

--- GameState.java
+++ GameState.java
@@ -1,20 +1,21 @@
-// Source code is decompiled from a .class file using FernFlower decompiler.
 import java.util.List;
 
+// GameState.java (to encapsulate game state)
 class GameState {
-   private int turn;
-   private List<Piece> pieces;
+    private int turn;
+    private List<Piece> pieces;
 
-   public GameState(int var1, List<Piece> var2) {
-      this.turn = var1;
-      this.pieces = var2;
-   }
+    public GameState(int turn, List<Piece> pieces) {
+        this.turn = turn;
+        this.pieces = pieces;
+    }
 
-   public int getTurn() {
-      return this.turn;
-   }
+    public int getTurn() {
+        return turn;
+    }
 
-   public List<Piece> getPieces() {
-      return this.pieces;
-   }
+    public List<Piece> getPieces() {
+        return pieces;
+    }
+
 }
