dd03c4740bdf8d5895c60d6fd69729daa667a8cd
SiaoWeiCheng <siaoweicheng@gmail.com>
2024-12-24 23:57:37 +0800

    Update GameLogic.java

--- GameLogic.java
+++ GameLogic.java
@@ -3,19 +3,35 @@
 class GameLogic {
     private Board board;
     private boolean blueTurn;
+    private int currentTurn;
+    private boolean rotated;
 
     public GameLogic(Board board) {
         this.board = board;
         this.blueTurn = true;
+        this.currentTurn = 1;
+        this.rotated = false;
     }
 
     public void switchTurn() {
         blueTurn = !blueTurn;
+        currentTurn++;
         board.rotate(); // Rotate the board after each turn
         board.updateBoardDisplay();  // Notify GUI to update the visual display after rotation
+        rotated = !rotated;  // Toggle rotation state
     }
 
     public boolean isBlueTurn() {
         return blueTurn;
     }
+
+    public int getCurrentTurn() {
+        return currentTurn;
+    }
+
+    public void setCurrentTurn(int turn) {
+        this.currentTurn = turn;
+        this.blueTurn = (turn % 2 == 1); // Assuming odd turns are blue's turn
+    }
+
 }
