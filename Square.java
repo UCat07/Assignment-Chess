ff604ddabc12fccc51ed4fa6dabbf0ff85dec9c6
UCat07 <soo.zw04@gmail.com>
2024-12-26 00:07:34 +0800

    Update Square.java

--- Square.java
+++ Square.java
@@ -1,32 +1,27 @@
-import javax.swing.*;
-import java.awt.*;
-
-class Square extends JButton {
-    private int x, y;
+class Square {
+    private int y;
+    private int x;
     private Piece piece;
 
     public Square(int y, int x) {
-        this.x = x;
         this.y = y;
+        this.x = x;
         this.piece = null;
-        setBackground((x + y) % 2 == 0 ? new Color(235, 236, 208) : new Color(119, 149, 86));
     }
 
     public void setPiece(Piece piece) {
         this.piece = piece;
-        setText(piece != null ? piece.getSymbol() : "");
-        setForeground(piece != null ? piece.getColor() : Color.BLACK);
     }
 
     public Piece getPiece() {
         return piece;
     }
 
-    public int getXPos() {
-        return x;
-    }
-
     public int getYPos() {
         return y;
+    }
+
+    public int getXPos() {
+        return x;
     }
 }
