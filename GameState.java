e787cd60aec8fea6b2aec926402a5f7c98004d20
SiaoWeiCheng <siaoweicheng@gmail.com>
2024-12-25 18:22:17 +0800

    Update GameState.java

--- GameState.java
+++ GameState.java
@@ -3,11 +3,17 @@ import java.util.List;
 // GameState.java (to encapsulate game state)
 class GameState {
     private int turn;
+    private boolean blueTurn;
     private List<Piece> pieces;
 
-    public GameState(int turn, List<Piece> pieces) {
+    public GameState(int turn, boolean blueTurn, List<Piece> pieces) {
         this.turn = turn;
+        this.blueTurn = blueTurn;
         this.pieces = pieces;
+    }
+
+    public boolean isBlueTurn() {
+        return blueTurn;
     }
 
     public int getTurn() {
