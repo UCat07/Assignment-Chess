906dc4ba7772f4194273a3255c48c8ab69f16465
banjueye <85421590+banjueye@users.noreply.github.com>
2025-01-13 15:38:15 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -38,6 +38,7 @@ public class Main {
         frame.add(boardGUI, BorderLayout.CENTER);
         frame.pack();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
         frame.setVisible(true);
 
         // Prompt the user to save the game when closing
